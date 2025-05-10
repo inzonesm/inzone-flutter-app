ecf8f87b5a09e449d6ade91058931d6672d2f6cb
Wonjae <wkim1128@umd.edu>
2025-05-10 13:52:57 -0400

    splash screen error fixed

--- lib/screen/auth/splash_screen.dart
+++ lib/screen/auth/splash_screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset(
+        child: Image.asset(
           'assets/splash.png',
         ),
       ),
