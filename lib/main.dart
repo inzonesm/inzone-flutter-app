b268c7392a121ad68eb914aa3eec46fc5c20d82d
Wonjae <wkim1128@umd.edu>
2025-05-09 22:01:11 -0400

    profile edit fixed

--- lib/main.dart
+++ lib/main.dart
@@ -11,42 +11,38 @@ import 'package:inzone/router/app_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:inzone/services/appsflyer_service.dart';
-import 'package:purchases_flutter/models/purchases_configuration.dart' show PurchasesConfiguration;
+import 'package:purchases_flutter/models/purchases_configuration.dart'
+    show PurchasesConfiguration;
 import 'dart:io' show Platform;
 
-import 'package:purchases_flutter/purchases_flutter.dart' show LogLevel, Purchases;
-
-
+import 'package:purchases_flutter/purchases_flutter.dart'
+    show LogLevel, Purchases;
 
 Future<void> initPlatformState() async {
   await Purchases.setLogLevel(LogLevel.debug);
 
   PurchasesConfiguration configuration;
   if (Platform.isAndroid) {
-    if (FirebaseAuth.instance.currentUser!=null) {
+    if (FirebaseAuth.instance.currentUser != null) {
       await Purchases.configure(
           PurchasesConfiguration('appl_veaMcyjzStDagTGHzLYMJiDVkWO')
-            ..appUserID = FirebaseAuth.instance.currentUser!.uid
-      );
+            ..appUserID = FirebaseAuth.instance.currentUser!.uid);
     }
   } else if (Platform.isIOS) {
-    if (FirebaseAuth.instance.currentUser!=null){
+    if (FirebaseAuth.instance.currentUser != null) {
       await Purchases.configure(
           PurchasesConfiguration('appl_veaMcyjzStDagTGHzLYMJiDVkWO')
-            ..appUserID = FirebaseAuth.instance.currentUser!.uid
-      );
+            ..appUserID = FirebaseAuth.instance.currentUser!.uid);
       print("CONFIGURED");
     }
-
   }
-
 }
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
 
   // Enable pending purchases on Android
-
 
   // Initialize Google Fonts
   GoogleFonts.config.allowRuntimeFetching = true;
@@ -58,7 +54,7 @@ void main() async {
   // Initialize AppsFlyerService
   final appsFlyerService = AppsFlyerService();
   await appsFlyerService.initialize();
-await initPlatformState();
+  await initPlatformState();
   String? advertisingId = await appsFlyerService.getAdvertisingId();
   print("The advertising ID is $advertisingId");
 
