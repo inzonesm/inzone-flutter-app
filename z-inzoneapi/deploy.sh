8ddb53ce227a3c2bab972f6436fe5c8d10265cb0
Yuan XY <43307952+yxydw98@users.noreply.github.com>
2025-10-17 13:40:33 -0400

    Integrate Gorse recommendation system with real-time sync

--- z-inzoneapi/deploy.sh
+++
@@ -1,3 +0,0 @@
-# chmod +x deploy.sh && ./deploy.sh
-gcloud builds submit --tag gcr.io/inzone-f93e4/inzoneapi
-gcloud run deploy inzoneapi --image gcr.io/inzone-f93e4/inzoneapi --region us-central1 --env-vars-file envs.yaml
