282b4832d59be49fbed2b3b217cbc1e87311f6ed
John Shim <jshim777@terpmail.umd.edu>
2025-11-18 18:01:07 -0500

    16 kb requirement & broken subscription button functionality fixed on android

--- android/app/src/main/kotlin/com/aadeshkheria/inzone/MainActivity.kt
+++ android/app/src/main/kotlin/com/aadeshkheria/inzone/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.aadeshkheria.inzone
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
