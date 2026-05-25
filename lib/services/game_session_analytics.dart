8ee951d2ba43713ee75c82cf69a00d6e1c69ece9
John Shim <132935050+jshim777@users.noreply.github.com>
2026-05-25 15:16:38 -0400

    Fix formatting issues in game_session_analytics.dart

--- lib/services/game_session_analytics.dart
+++ lib/services/game_session_analytics.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:simula_ads/simula_ads.dart';
-
+ 
 class GameSessionAnalytics {
   static final FirebaseFirestore _db = FirebaseFirestore.instance;
 
